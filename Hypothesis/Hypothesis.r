16f8d6b378fc8368e63c610bee1742d3eb501216
cmm0072@mix.wvu.edu <cmm0072@mix.wvu.edu>
2021-03-05 10:33:28 -0500

    CoreyMckinney Hypothesis

---
+++ Hypothesis/Hypothesis.r
@@ -0,0 +1,3 @@
+#'There is a longer latency seen in mice raised by heterospecific parents relative to those raised by conspeific parents.'
+#'Another hypothesis if the above one is too similar to what they did - There is a higher percent chance that offspring raised by conspecific parents have offspring of their own than heterospecific raised offspring." 
+d = read.table (file = "Peromyscus_matechoice.txt", header = TRUE, sep = "\t")
